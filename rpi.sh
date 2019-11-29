0b37588e79ee3e4945f776fe595d60c2dddd5691
mashimine <mauricio@caylent.com>
2019-11-29 11:39:43 -0300

    add git https url

--- rpi.sh
+++ rpi.sh
@@ -19,7 +19,7 @@ apt-get update \
 && apt-get install -y git
 
 # Clone repo
-git clone git@github.com:mauhftw/rpi.git \
+git clone https://github.com/mauhftw/rpi
 
 # Setup system dependencies
 ${TOOL_ROOT}/rpi/setup.sh
